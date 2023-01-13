0164bf6530f23990a2c09047bdd818ed87aa2495
ALX <3496916+alexplong@users.noreply.github.com>
2023-01-13 09:46:05 -0500

    - Added main component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,25 +1,41 @@
 /** @format */
 
 import React from "react";
+import memesData from "../memesData";
 
 function Meme() {
+  const [memeImage, setMemeImage] = React.useState(
+    "https://i.imgflip.com/30b1gx.jpg"
+  );
+
+  const getMemeImage = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const memesURL = memesArray[randomNumber].url;
+    setMemeImage(memesURL);
+    // return memesURL
+  };
+
   return (
     <section className="form">
-      <form>
-        <div className="form--input_wrapper">
-          <input
-            type="text"
-            className="form--input_left"
-            placeholder="Top text"
-          />
-          <input
-            type="text"
-            className="form--input_right"
-            placeholder="Bottom text"
-          />
-        </div>
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      <div className="form--input_wrapper">
+        <input
+          type="text"
+          className="form--input_left"
+          placeholder="Top text"
+        />
+        <input
+          type="text"
+          className="form--input_right"
+          placeholder="Bottom text"
+        />
+      </div>
+      <button className="form--button" onClick={getMemeImage}>
+        Get a new meme image 🖼
+      </button>
+      <div className="meme--container">
+        <img alt="meme" src={memeImage} className="meme" />
+      </div>
     </section>
   );
 }
