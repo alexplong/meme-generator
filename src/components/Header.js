545aa7d9c0d33d2a33beebc56a971c023314b43f
ALX <3496916+alexplong@users.noreply.github.com>
2023-01-13 09:45:22 -0500

    - Added main component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ function Header() {
   return (
     <header>
       <img alt="meme logo" src={trollface} className="header--icon" />
-      <h1 className="header--title">Meme Generator</h1>
-      <h2 className="header--subtitle">React Course - Project 3</h2>
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--subtitle">React Course - Project 3</h4>
     </header>
   );
 }
